eef80881f409f8fa289fe45dd6a7b15799c51040
abdullah dağlı <abdullah.dagli@outlook.com>
2022-09-13 00:24:03 +0300

    Technology Commands

--- src/demoProjects/Kodlama.io.Devs/Devs.Application/Features/Technologies/Dtos/DeletedTechnologyDto.cs
+++ src/demoProjects/Kodlama.io.Devs/Devs.Application/Features/Technologies/Dtos/DeletedTechnologyDto.cs
@@ -10,7 +10,7 @@ namespace Devs.Application.Features.Technologies.Dtos
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public string SPLName { get; set; }
+		public string SPLName { get; set; } 
 
 	}
 }
